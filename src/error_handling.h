8822acdc37767b0c414389e7063ec5a4073d68b8
Brooke Moss <me@brookem.dev>
2025-04-25 14:01:40 -0400

    Adjust error_handling imports

--- src/error_handling.h
+++ src/error_handling.h
@@ -1,12 +1,27 @@
+#pragma once
+
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <linux/limits.h>
+#include <sched.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <unistd.h>
 
 struct child_config {
-    int argc;
-    uid_t uid;
-    int fd;
-    char* hostname;
-    char** argv;
-    char* mount_dir;
+  int argc;
+  uid_t uid;
+  int fd;
+  char* hostname;
+  char** argv;
+  char* mount_dir;
 };
 
 /**
